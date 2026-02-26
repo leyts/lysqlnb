b6ab5cbbc272115f80e5f586e390b2f5c699ae56
Leyton Addison-Roach <git@laddisonroach.com>
2026-02-26 03:14:22 +0000

    test: add test case for blank cell value

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,6 +7,7 @@ from lysqlnb.models import NotebookCellKind, NotebookCellLanguage, NotebookData
 if TYPE_CHECKING:
     from pathlib import Path
 
+
 @pytest.fixture
 def notebook(shared_datadir: Path) -> NotebookData:
     return NotebookData.from_file(shared_datadir / "test_notebook.sqlnb")
@@ -35,6 +36,15 @@ def test_notebook_cell_value(notebook: NotebookData):
         "HAVING\n"
         "    AVG(salary) > 5000;"
     )
+
+
+def test_notebook_cell_value_blank(shared_datadir: Path):
+    notebook: NotebookData = NotebookData.from_file(
+        shared_datadir / "test_notebook_blank_cell.sqlnb"
+    )
+    assert notebook.cells[0].value == "# The cell below is blank."
+    assert notebook.cells[1].value == ""
+    assert notebook.cells[2].value == "# The cell above is blank."
 
 
 def test_notebook_cell_language(notebook: NotebookData):
